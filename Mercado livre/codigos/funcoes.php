6da7c270ea8d01f5ffaf451c7bbcc51871d652ba
atam4ra <atamaratavares86@gmail.com>
2025-04-10 09:53:58 -0300

    deletarusuario, cadastrarvendedor
    atamara

--- Mercado livre/codigos/funcoes.php
+++ Mercado livre/codigos/funcoes.php
@@ -11,4 +11,27 @@ function editarUsuario($conexao, $email, $senha, $tipo, $idusuario) {
     return $funcionou;
 };
 
+function deletarUsuario($conexao, $idusuario) {
+    $sql = "DELETE FROM tb_usuario WHERE idusuario= ?";
+    $comando = mysqli_prepare($conexao, $sql);
+
+    mysqli_stmt_bind_param($comando, 'i', $idusuario);
+    $funcionou = mysqli_stmt_execute($comando);
+
+    mysqli_stmt_close($comando);
+    
+    return $funcionou; //true ou false
+};
+
+function cadastrarVendedor($conexao, $nome, $cpf, $endereco) {
+    $sql = "INSERT INTO tb_vendedor (nome, cpf, endereco) VALUES (?, ?, ?)";
+    $comando = mysqli_prepare($conexao, $sql);
+    
+    mysqli_stmt_bind_param($comando, 'sss', $nome, $cpf, $endereco);
+    
+    $funcionou = mysqli_stmt_execute($comando);
+    
+    mysqli_stmt_close($comando);
+    return $funcionou;
+};
 ?>
