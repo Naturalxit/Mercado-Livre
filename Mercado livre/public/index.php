f6120cb44936944c698a62fb904ce344e74f343d
Naturalxit <121978266+Naturalxit@users.noreply.github.com>
2025-04-10 07:59:04 -0300

    Exemplo do inicio
    Roger

---
+++ Mercado livre/public/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/estilo.css">
+
+    <title> Pagina Inicial </title>
+    <div class="Pagina Inicial"></div>
+</head>
+<body>
+<div class="header">
+            <iframe src="./assets/preto.png" class="ifram"></iframe>
+        </div>
+
+
+</body>
+</html>
---
+++ Mercado livre/public/menulogin.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/estilo.css">
+
+    <title> Login Usuário</title>
+</head>
+<body class="indexaum">
+    <div class="Login Usuário">
+        <form class="login" action= ""method="post">
+        <h1>Login Usuário</h1>
+        <b>Email:</b> <br>
+        <input type="text" name="email" class="quadradinho"> <br><br>
+       <b>Senha:</b> <br>
+        <input type="password" name="senha"class="quadradinho"> <br><br>
+
+        <input class="button" type="submit" value="Login">
+    </div>
+</body>
+</html>
