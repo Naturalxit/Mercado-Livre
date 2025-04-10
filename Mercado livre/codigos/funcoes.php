f3087915408c84f1ad7ace04dbed232abc197d1e
atam4ra <atamaratavares86@gmail.com>
2025-04-10 10:20:25 -0300

    atamara
    alterei o cadastrarvendedor, fiz o listarvendedor, salvarproduto, cadastrarpedido

--- Mercado livre/codigos/funcoes.php
+++ Mercado livre/codigos/funcoes.php
@@ -23,15 +23,79 @@ function deletarUsuario($conexao, $idusuario) {
     return $funcionou; //true ou false
 };
 
-function cadastrarVendedor($conexao, $nome, $cpf, $endereco) {
-    $sql = "INSERT INTO tb_vendedor (nome, cpf, endereco) VALUES (?, ?, ?)";
+function cadastrarVendedor($conexao, $nome, $cpf, $endereco, $telefone) {
+    $sql = "INSERT INTO tb_vendedor (nome, cpf, endereco, telefone) VALUES (?, ?, ?, ?)";
     $comando = mysqli_prepare($conexao, $sql);
     
-    mysqli_stmt_bind_param($comando, 'sss', $nome, $cpf, $endereco);
+    mysqli_stmt_bind_param($comando, 'ssss', $nome, $cpf, $endereco);
     
     $funcionou = mysqli_stmt_execute($comando);
     
     mysqli_stmt_close($comando);
     return $funcionou;
 };
-?>
+
+function listarVendedor($conexao) {
+    $sql = "SELECT * FROM tb_vendedor";
+    $comando = mysqli_prepare($conexao, $sql);
+
+    mysqli_stmt_execute($comando);
+    $resultado = mysqli_stmt_get_result($comando);
+
+    $lista_vendedores = [];
+    while ($vendedor = mysqli_fetch_assoc($resultado)) {
+        $lista_vendedores[] = $vendedor;
+    }
+
+    mysqli_stmt_close($comando);
+    return $lista_vendedores;
+};
+
+function salvarVendedor($conexao, $nome, $cpf, $endereco, $telefone) {
+    $sql = "INSERT INTO tb_vendedor (nome, cpf, endereco, telefone) VALUES (?, ?, ?, ?)";
+    $comando = mysqli_prepare($conexao, $sql);
+    
+    mysqli_stmt_bind_param($comando, 'ssss', $nome, $cpf, $endereco, $telefone);
+    
+    $funcionou = mysqli_stmt_execute($comando);
+    
+    mysqli_stmt_close($comando);
+    return $funcionou;
+};
+
+function salvarProduto($conexao, $nome, $tipo, $estado, $valor, $estoque, $descricao, $status) {
+    $sql = "INSERT INTO tb_produto (nome, tipo, estado, valor, estoque, descricao, status) VALUES (?, ?, ?, ?, ?, ?, ?)";
+    $comando = mysqli_prepare($conexao, $sql);
+    
+    mysqli_stmt_bind_param($comando, 'sssdiss', $nome, $tipo, $estado, $valor, $estoque, $descricao, $status);
+    
+    $funcionou = mysqli_stmt_execute($comando);
+    
+    mysqli_stmt_close($comando);
+    return $funcionou;
+};
+
+function cadastrarPedido($conexao, $total, $Produto_idproduto, $Cliente_idcliente) {
+    $sql = "INSERT INTO tb_pedido (total, Produto_idproduto, Cliente_idcliente) VALUES (?, ?, ?)";
+    $comando = mysqli_prepare($conexao, $sql);
+    
+    mysqli_stmt_bind_param($comando, 'dii', $total, $Produto_idproduto, $Cliente_idcliente);
+    
+    $funcionou = mysqli_stmt_execute($comando);
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+    mysqli_stmt_close($comando);
+    return $funcionou;
+};
+
+//function editarProduto($conexao, $nome, $tipo, $preco_compra, $preco_venda, $margem_lucro, $quantidade, $idproduto) {
+    //$sql = "UPDATE tb_produto SET nome=?, tipo=?, preco_compra=?, preco_venda=?, margem_lucro=?, quantidade=? WHERE idproduto=?";
+    //$comando = mysqli_prepare($conexao, $sql);
+    
+    //mysqli_stmt_bind_param($comando, 'ssdddii', $nome, $tipo, $preco_compra, $preco_venda, $margem_lucro, $quantidade, $idproduto);
+    
+    //$funcionou = mysqli_stmt_execute($comando);
+    
+    //mysqli_stmt_close($comando);
+    //return $funcionou;
+//};
+ ?>
