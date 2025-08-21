e5bd6793469057be3958d9f92619b097267d2a35
atam4ra <atamaratavares86@gmail.com>
2025-08-21 14:24:50 -0300

    atamara
    css oferta link

---
+++ MercadoLivre/public/menuprincipal.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+  <meta charset="UTF-8">
+  <title>Clickaê Loja</title>
+  <link rel="stylesheet" href="./css/menuprincipal.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+</head>
+<body>
+  <header>
+    <div class="topo">
+      <div class="logo">
+        <i class="fa-solid fa-hand-pointer"></i> <span>Clickaê</span>
+
+
+
+      </div>
+      <div class="busca">
+        <input type="text" placeholder="Buscar produto...">
+        <button><i class="fa-solid fa-magnifying-glass"></i></button>
+      </div>
+
+
+      <div class="acoes">
+        <div class="frete">Frete Grátis</div>
+        <div class="conta">
+        <span><a href="perfil.php"><i class="fa-solid fa-user"></i></span>
+          <span><a href="menuregistrar.php">Criar Conta</a> | <a href="index.php">Entrar</a></span>
+        </div>
+
+
+        <div class="carrinho">
+          <i class="fa-solid fa-cart-shopping"></i>
+        </div>
+      </div>
+    </div>
+
+
+<!-- menu categorias -->
+
+    <nav class="menu-inferior">
+  <div class="dropdown">
+    <a href="#" class="dropbtn">Categorias</a>
+    <div class="dropdown-content">
+      <a href="veiculo.php">Veículos</a>
+      <a href="eletrodomestico.php">Eletrodomésticos</a>
+      <a href="moda.php">Moda</a>
+      <a href="petshop.php">Pet Shop</a>
+      <a href="brinquedos.php">Brinquedos</a>
+      <a href="imoveis.php">Imóveis</a>
+    </div>
+  </div>
+
+    </div>
+  </div>
+  <a href="ofertas.php" class="teste">Ofertas</a>
+  <a href="#" class="teste" >Cupons</a>
+  <a href="#" class="teste">Contato</a>
+</nav>
+  </header>
+</body>
+</html>
