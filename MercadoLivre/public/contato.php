2835dd8289c648ef8ef4d3dde45c6adee65d3d98
atam4ra <atamaratavares86@gmail.com>
2025-08-25 16:45:35 -0300

    Atamara
    php contato e css contato

--- MercadoLivre/public/contato.php
+++ MercadoLivre/public/contato.php
@@ -8,26 +8,60 @@
     
 </head>
 <body>
-  
+  <div class="topo">
 
-    <div class="topo">
+    <h1 class="center">Como podemos te ajudar?</h1>
 
-      <div class="logo">
-
-        <i class="fa-solid fa-hand-pointer"></i>
-
-      </div>
-
-
-        <h1>Como podemos te ajudar?</h1>
+  </div>
 
         <div class="busca">
 
-        <input type="text" placeholder="Buscar produto...">
-        <button><i class="fa-solid fa-magnifying-glass"></i></button>
+          <input type="text" placeholder="Buscar produto...">
+
+          <button><i class="fa-solid fa-magnifying-glass"></i></button>
+
+        </div>
+
+  <div class="conteudo">
+
+
+
+      Compras <br><br>
+
+      <div class="conteudo1">
+
+          Administrar e cancelar compras <br><br>
+          Pagar, rastrear envios,alterar,reclamar ou cancelar compras
+
+      </div>
+      <br>
+      <div class="conteudo1">
+
+          Devoluções e reembolsos<br><br>
+          Devolver um produto ou consultar por estorno de compra
+
+      </div>
+      <br>
+            <div class="conteudo1">
+
+          Perguntas frequentes sobre compras <br><br>
+
+      </div>
+
+      <br>
+
+      Venda <br><br>
+
+      <div class="conteudo1">
+
+          Reputação, vendas e envio <br><br>
+          Perguntar sobre sua reputação, envio, pagamento ou devolução
+
       </div>
 
 
+
+  </div>
 
 </body>
 </html>
