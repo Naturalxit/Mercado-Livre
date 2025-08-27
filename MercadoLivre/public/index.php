450f50e1bc26c4b9966bbe3863b66ff188eed5c0
lorena367 <lorenatavares740@gmail.com>
2025-08-27 09:32:08 -0300

    Lorena
    css index

--- MercadoLivre/public/index.php
+++ MercadoLivre/public/index.php
@@ -1,22 +1,29 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="./css/index.css">
+    <title>Clickaê Login</title>
 </head>
 <body>
-    <h1>Acesso ao sistema</h1>
+    <div class="centro">
+        <div class="caixa">
+            <h1>Clickaê Login</h1>
+            <form action="verificarLogin.php" method="post">
+                E-mail: <br>
+                <input type="email" name="email"> <br><br>
+                Senha: <br>
+                <input type="password" name="senha"> <br><br>
+                    <div class="acesso">
+                <a href="formUsuario.php">Primeiro acesso</a> <br><br>
+                    </div>
+                    <div class="botao">
+                        <input type="submit" value="Logar" class="botao">
 
-    <form action="verificarLogin.php" method="post">
-        E-mail: <br>
-        <input type="text" name="email"> <br><br>
-        Senha: <br>
-        <input type="text" name="senha"> <br><br>
-
-        <a href="formUsuario.php">Primeiro acesso</a> <br><br>
-
-        <input type="submit" value="Acessar">
-    </form>
+                    </div>      
+            </form>
+        </div>
+    </div>
 </body>
 </html>
